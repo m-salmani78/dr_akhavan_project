e0c2981de8c34ee41fa35a7bf3211298cc4e8afd
momasal <mmsz78@yahoo.com>
2022-03-19 15:39:53 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'pages/home_page/home_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      supportedLocales: [Locale('fa')],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: HomePage(),
+    );
+  }
+}
